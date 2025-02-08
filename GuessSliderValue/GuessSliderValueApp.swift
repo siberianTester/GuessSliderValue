d6c8bd51944bc6496e21ec1aed485d6eae665359
Yaroslav Malygin <ya.malygin@team.superjob.ru>
2025-02-08 18:44:30 +0300

    Initial Commit

---
+++ GuessSliderValue/GuessSliderValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessSliderValueApp.swift
+//  GuessSliderValue
+//
+//  Created by Yaroslav Malygin on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessSliderValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
